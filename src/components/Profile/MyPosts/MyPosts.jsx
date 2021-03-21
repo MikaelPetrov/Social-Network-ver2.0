4561369a6cb1367d1de152a4b9a02ea364a87577
Mikael Petrov <bmao@inbox.ru>
2021-03-21 14:01:59 +0300

    lesson 74 (redux-form field validation) has been completed

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { AddNewPostReduxForm } from './AddPostForm/AddPostForm';
 
 
 const MyPosts = (props) => {
@@ -19,23 +19,5 @@ const MyPosts = (props) => {
         </div>
     )
 }
-
-const AddNewPostForm = (props) => {
-    return <form onSubmit={props.handleSubmit}>
-        <div>
-            <Field
-                component='textarea'
-                name='newPostText'
-                placeholder='Enter your post' />
-        </div>
-        <div>
-            <button>Add post</button>
-        </div>
-    </form>
-}
-
-const AddNewPostReduxForm = reduxForm({
-    form: 'profileAddNewPostForm',
-})(AddNewPostForm);
 
 export default MyPosts;
