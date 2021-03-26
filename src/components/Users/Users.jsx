9b300878514f51a14a6365f7dae9a136654db70f
Mikael Petrov <bmao@inbox.ru>
2021-03-26 22:45:39 +0300

    Revert "lesson 84 (paginator, ) has been completed"
    This reverts commit 4c416870268898a4bfafa88860c7f3bdb1a0457b.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,7 +4,7 @@ import User from './User';
 
 let Users = React.memo(({ totalCountUsers, pageSize, currentPage, onPageChanged, ...props }) => {
     return <div>
-        <Paginator totalItemsCount={totalCountUsers} pageSize={pageSize} currentPage={currentPage} onPageChanged={onPageChanged} />
+        <Paginator totalCountUsers={totalCountUsers} pageSize={pageSize} currentPage={currentPage} onPageChanged={onPageChanged} />
         <User users={props.users} isFollowingProgress={props.isFollowingProgress} unfollowThunkCreator={props.unfollowThunkCreator} followThunkCreator={props.followThunkCreator} />
     </div>
 })
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react';
-import s from './Paginator.module.css';
-import cn from 'classnames';
+import s from './Paginator.module.css'
 
-let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
+let Paginator = ({ totalCountUsers, pageSize, currentPage, onPageChanged }) => {
 
-    let pagesCount = Math.ceil(totalItemsCount / pageSize);
+    let pagesCount = Math.ceil(totalCountUsers / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) { pages.push(i) };
 
-    let portionCount = Math.ceil(pagesCount / portionSize);
-    let [portionNumber, setPortionNumber] = useState(1);
-    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-    let rightPortionPageNumber = portionNumber * portionSize;
-
-    return <div className={s.paginator}>
-        {portionNumber > 1 && <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button>}
-        {pages
-            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-            .map(p => {
-                return <div className={s.pagesBlock}>
-                    <span
-                        className={cn({ [s.selectedPage]: currentPage === p }, s.pageNumber)}
-                        key={p}
-                        onClick={(e) => { onPageChanged(p) }}
-                    >{p}</span>
-                </div>
-            })
-        }
-        {portionCount > portionNumber && <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button>}
+    return <div>
+        {pages.map(p => {
+            return <span
+                className={currentPage === p && s.selectedPage}
+                onClick={(e) => {
+                    onPageChanged(p)
+                }}>{p}</span>
+        })}
     </div>
 
 }
