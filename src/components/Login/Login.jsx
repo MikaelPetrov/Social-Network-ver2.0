a72a4d3832ba34f6d710b5541efddb45e112b494
Mikael Petrov <bmao@inbox.ru>
2021-03-28 09:34:04 +0300

    lesson-88 (captcha) has been completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,11 +6,13 @@ import { required } from "../../utils/validators/validators";
 import { createField, Input } from "../common/FormControl/FormControl";
 import s from '../common/FormControl/FormControl.module.css';
 
-const LoginForm = ({ handleSubmit, error }) => {
+const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
     return <form onSubmit={handleSubmit}>
         {createField('Email', 'email', Input, [required])}
         {createField('Password', 'password', Input, [required], { type: 'password' })}
         {createField(null, 'rememberMe', Input, [], { type: 'checkbox' }, 'remember me')}
+        {captchaUrl && <img src={captchaUrl} />}
+        {captchaUrl && createField('Symbols from image', 'captcha', Input, [required])}
         {error && <div className={s.formSummaryError}>{error}</div>}
         <div>
             <button>Login</button>
@@ -22,18 +24,19 @@ const LoginReduxForm = reduxForm({ form: 'loginForm' })(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.loginThunkCreator(formData.email, formData.password, formData.rememberMe);
+        props.loginThunkCreator(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
     if (props.isAuth) {
         return <Redirect to={'/profile'} />
     }
     return <div>
         <h1>Log In</h1>
-        <LoginReduxForm onSubmit={onSubmit} />
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
     </div>
 }
 
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 const mapDispatchToProps = {
