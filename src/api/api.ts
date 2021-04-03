6590d342187c0c4f75930c50e9db65773e5edebc
Mikael Petrov <bmao@inbox.ru>
2021-04-03 20:19:33 +0300

    2.0/lesson-006 (React + Redux + TypeScript, axios, api, enum and Promise)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
-import * as axios from "axios";
+import { ProfileType } from './../types/types';
+import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
@@ -16,39 +17,39 @@ export const usersAPI = {
                 return response.data
             })
     },
-    followProcess(userId) {
+    followProcess(userId: number | null) {
         return instance
             .post(`follow/${userId}`)
     },
-    unfollowProcess(userId) {
+    unfollowProcess(userId: number | null) {
         return instance
             .delete(`follow/${userId}`)
     },
-    getUserProfile(userId) {
+    getUserProfile(userId: number | null) {
         return profileAPI.getUserProfile(userId)
     }
 }
 
 export const profileAPI = {
-    getUserProfile(userId) {
+    getUserProfile(userId: number | null) {
         return instance
             .get(`profile/` + userId)
     },
-    getUserStatus(userId) {
+    getUserStatus(userId: number | null) {
         return instance
             .get(`profile/status/` + userId)
     },
-    updateUserStatus(status) {
+    updateUserStatus(status: string) {
         return instance
             .put(`profile/status`, { status: status })
     },
-    saveProfilePhoto(photoFile) {
+    saveProfilePhoto(photoFile: any) {
         const formData = new FormData()
         formData.append('image', photoFile)
         return instance
             .put(`profile/photo`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
     },
-    saveProfileData(profile) {
+    saveProfileData(profile: ProfileType) {
         return instance
             .put(`profile`, profile)
     }
@@ -57,11 +58,13 @@ export const profileAPI = {
 export const authAPI = {
     authMe() {
         return instance
-            .get(`auth/me`)
+            .get<MeResponseType>(`auth/me`)
+            .then(response => response.data)
     },
-    authLogin(email, password, rememberMe = false, captcha = null) {
+    authLogin(email: string, password: string, rememberMe = false, captcha: null | string = null) {
         return instance
-            .post(`auth/login`, { email, password, rememberMe, captcha })
+            .post<LoginResponseType>(`auth/login`, { email, password, rememberMe, captcha })
+            .then(response => response.data)
     },
     authLogout() {
         return instance
@@ -74,4 +77,29 @@ export const securityAPI = {
         return instance
             .get(`security/get-captcha-url`)
     }
+}
+
+//  MeResponseType, LoginResponseType and enum
+type MeResponseType = {
+    data: {
+        id: number
+        email: string
+        login: string
+    }
+    resultCode: ResultCodeEnum
+    messages: Array<string>
+}
+
+type LoginResponseType = {
+    data: {
+        userId: number
+    }
+    resultCode: ResultCodeEnum
+    messages: Array<string>
+}
+
+export enum ResultCodeEnum {
+    Success = 0,
+    Error = 1,
+    Captcha = 10
 }
